29372e547d4106ea1eb7af7de32eab7f6bccee84
CristianJT <cjtodorovic@outlook.com>
2016-07-30 17:32:23 -0300

    Create project

---
+++ Bairoletto/Bairoletto/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Web.Http;
+
+namespace Bairoletto
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            var DefFormatter = new Newtonsoft.Json.JsonSerializerSettings();
+
+            DefFormatter.Formatting = Newtonsoft.Json.Formatting.None;
+            DefFormatter.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+            DefFormatter.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+            DefFormatter.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+            Newtonsoft.Json.JsonConvert.DefaultSettings = () => DefFormatter;
+            config.Formatters.JsonFormatter.SerializerSettings = DefFormatter;
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            //Remove XML Formatter
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
---
+++ Bairoletto/Bairoletto/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bairoletto.WebApiApplication" Language="C#" %>
