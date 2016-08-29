c321246efdf207e9f5bfa3b37fdf11ae0bbeff52
CristianJT <cjtodorovic@outlook.com>
2016-08-28 21:17:37 -0300

    Create custom css. Create component for panel-inside

---
+++ Bairoletto/Bairoletto/bairoletto/core/layout/bai-panel-inside/bai-panel-inside.component.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app.core')
+        .component('baiPanelInside', {
+            templateUrl: '/bairoletto/core/layout/bai-panel-inside/bai-panel-inside.component.html',
+            controller: BaiPanelInsideController,
+            transclude: {
+                'info': '?baiInfo'
+            },
+            bindings: {
+                baiMainColor: '<',
+                baiOpenedColor: '<',
+                baiOpen: '<',
+                baiOnClick: '&'
+            }
+        });
+
+    function BaiPanelInsideController() {
+        var vm = this;
+        vm.open = open;
+
+        vm.opened_subtitle_style = {
+            'border-top': '1px solid ' + vm.baiOpenedColor
+        }
+        vm.opened_icon_style = {
+            'color': vm.baiOpenedColor
+        }
+
+        function open() {
+            vm.baiOnClick();
+        }
+    }
+
+})();
