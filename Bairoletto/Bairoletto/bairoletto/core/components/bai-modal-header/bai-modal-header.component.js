7ea19128c3ab348018d59cd9f80d2161e3da770e
CristianJT <cjtodorovic@outlook.com>
2016-09-18 15:03:23 -0300

    Crear modal header component

---
+++ Bairoletto/Bairoletto/bairoletto/core/components/bai-modal-header/bai-modal-header.component.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app.core')
+        .component('baiModalHeader', {
+            templateUrl: '/bairoletto/core/components/bai-modal-header/bai-modal-header.component.html',
+            controller: ModalHeaderController,
+            bindings: {
+                baiTitle: '<',
+                baiOnClick: '&',
+            }
+        });
+
+    function ModalHeaderController() {
+        var vm = this;
+        vm.close = close;
+
+        function close () {
+            vm.baiOnClick();
+        }
+    }
+
+})();
