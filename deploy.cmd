68e3d676e4b8de322b3653cb3ba89aa1d41e26f6
mngobbi <mgobbi@tecnom.com.ar>
2016-10-30 18:28:02 -0300

    npm install fix

--- deploy.cmd
+++ deploy.cmd
@@ -91,7 +91,7 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 :: 4. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install --production
+  call :ExecuteCmd npm install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
